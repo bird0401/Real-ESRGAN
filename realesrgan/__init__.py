bff465eebf130d6fb071b4b97527e7e87ab065ba
bird <53257509+bird0401@users.noreply.github.com>
2022-06-05 03:26:21 +0900

    Update __init__.py

--- realesrgan/__init__.py
+++ realesrgan/__init__.py
@@ -1,6 +1,12 @@
 # flake8: noqa
+from os.path import dirname, abspath
+import sys
+
+parent_dir = dirname(abspath(__file__))
+sys.path.append(parent_dir)
+
 from .archs import *
 from .data import *
 from .models import *
 from .utils import *
-from .version import *
+# from .version import *
